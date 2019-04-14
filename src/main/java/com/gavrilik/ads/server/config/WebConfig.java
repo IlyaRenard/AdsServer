2d4f8f3146b60556e433d5f262e9b986fcb8d20f
Your Name <you@example.com>
2019-04-14 22:13:17 +0300

    DB

---
+++ src/main/java/com/gavrilik/ads/server/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.gavrilik.ads.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.gavrilik.ads.server")
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
